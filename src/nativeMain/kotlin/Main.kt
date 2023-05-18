e326a6d0dba33a3e9c259c2d626bbdf8cdd40223
Josh Jones <ohblonddev@gmail.com>
2023-05-18 23:29:06 +0100

    Modify output

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -2,13 +2,21 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.int
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 class HappyNumber : CliktCommand() {
     private val range by option(help = "End inclusive range of numbers to test").int().default(1000000)
 
+    @OptIn(ExperimentalTime::class)
     override fun run() {
-        val distinctHappyNumbers = countDistinctHappyNumbers(IntRange(0, range))
-        println("happy numbers: $distinctHappyNumbers")
+        var distinctHappyNumbers: Int
+        val duration: Duration = measureTime {
+            distinctHappyNumbers = countDistinctHappyNumbers(IntRange(0, range))
+        }
+
+        println("count: $distinctHappyNumbers\ntime: ${duration.inWholeMilliseconds}ms")
     }
 }
 
